91e63b8dfb14f2bf42314d394c4c663d337c0af1
Alen Ribic <alen@sweepsouth.com>
2018-05-15 00:30:48 +0200

    Made params more readable.

--- src/Actions/FetchCoinData.js
+++ src/Actions/FetchCoinData.js
@@ -16,25 +16,27 @@ export default function FetchCoinData() {
       .then(res => {
         const symbols = Object.keys(res.data.data).map((key) => res.data.data[key].symbol).join(',')
         axios.get(`${cryptopanicApiBaseURL}/posts/?auth_token=6225b85be3b432230592a83bd317e4aa46ff4bc7&public=true&currencies=${symbols}`)
-          .then(res2 => {
-            if (res2.data.next) {
-              axios.get(res2.data.next)
-                .then(res3 => {
-                  if (res3.data.next) {
-                    axios.get(res3.data.next)
-                      .then(res4 => {
-                        res.data['news'] = res2.data.results.concat(
-                          res3.data.results.concat(res4.data.results)
+          .then(resPg1 => {
+            if (resPg1.data.next) {
+              axios.get(resPg1.data.next)
+                .then(resPg2 => {
+                  if (resPg2.data.next) {
+                    axios.get(resPg2.data.next)
+                      .then(resPg3 => {
+                        res.data['news'] = resPg1.data.results.concat(
+                          resPg2.data.results.concat(
+                            resPg3.data.results
+                          )
                         )
                         dispatch({type: FETCH_COIN_DATA_SUCCESS, payload: res.data})
                       })
                   } else {
-                    res.data['news'] = res2.data.results.concat(res3.data.results)
+                    res.data['news'] = resPg1.data.results.concat(resPg2.data.results)
                     dispatch({type: FETCH_COIN_DATA_SUCCESS, payload: res.data})
                   }
                 })
             } else {
-              res.data['news'] = res2.data.results
+              res.data['news'] = resPg1.data.results
               dispatch({type: FETCH_COIN_DATA_SUCCESS, payload: res.data})
             }
           })
